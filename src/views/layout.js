136d4a38637aae47cd13b31865966b82a18d33e9
Kai Hendry <hendry@iki.fi>
2021-02-18 10:43:44 +0800

    Following https://dev.to/pchinjr/serverless-login-with-openjs-architect-part-1-2cpj

---
+++ src/views/layout.js
@@ -0,0 +1,17 @@
+// src/views/layout.js
+module.exports = function layout(params) {
+
+  let logout = `<a href=/logout>Logout</a> | <a href=/admin>Admin</a>`
+
+  let notAuthed = `<a href=/login>Login</a> | <a href=/register>Register</a> | <a href=/reset>Reset Password</a>`
+
+  return `
+  <!doctype html>
+  </html>
+  <h1> My Login </h1>
+  ${ params.account ? logout: notAuthed}
+  ${ params.body}
+  </html>
+  `
+}
+
